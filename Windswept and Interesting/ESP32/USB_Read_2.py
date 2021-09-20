e39d18ded22fec7d1ab400431022aeb617fae8cf
tblackwood2r <91059665+tblackwood2r@users.noreply.github.com>
2021-09-20 15:02:22 +0100

    Updating USB Read

--- Windswept and Interesting/ESP32/USB_Read_2.py
+++ Windswept and Interesting/ESP32/USB_Read_2.py
@@ -30,7 +30,7 @@ xs_yaw = [np.NaN] * log_size
 
 # Set the expected chart limits, 
 # x-number of data points and y-data range
-ax.set_ylim([0, log_size])
+ax.set_ylim([0, log_size - 1])
 
 # Setup the lines that are going to be streamed 
 # with names for legend
@@ -45,7 +45,7 @@ ax.legend(bbox_to_anchor=(2, 1))
 
 ax1 = fig.add_subplot(212)
 ax1.set_ylim([-2, 2])
-ax1.set_xlim([0, 200])
+ax1.set_xlim([0, log_size])
 
 ys_x = [np.NaN] * log_size
 ys_y = [np.NaN] * log_size
@@ -77,12 +77,12 @@ writer.writerow(header)
 def get_readings(ser, now):
     # Function to get the reading from the serial port 
     # (string) and clean it into a numpy array.
-    arr = np.array([np.NaN] * 6)
+    arr = np.array([np.NaN] * 7)
     if serial:
         line = ser.readline() # read bytes until line-ending
         line = line.decode(encoding='UTF-8') # convert to string    
         split_line = line.splitlines()[0].split(',')
-        if len(split_line) == 6:
+        if len(split_line) == 7:
             try: 
                 arr = np.array(split_line).astype(np.float)
             except:
@@ -97,38 +97,43 @@ def get_readings(ser, now):
         print('No Serial Connection')        
     return arr
 
-def animate(i, ser, now, xs_roll, xs_pitch, xs_yaw, ys_x, ys_y, ys_z):
-    
+def animate(i, ser, xs_roll, xs_pitch, xs_yaw, ys_x, ys_y, ys_z):
+    global now
+
     # Get USB readings from function above
     pos = get_readings(ser, now)
     # print(pos)
+    tmp_now = dt.datetime.now()
+    writer.writerow([tmp_now] + list(pos[0:5]))
+    # update = now + dt.timedelta(milliseconds = 200)
+
+    if pos[6] == 11: 
+        xs_roll.append(pos[0]/180*np.pi)
+        xs_pitch.append(pos[1]/180*np.pi)
+        xs_yaw.append(pos[2]/180*np.pi)
+        
+        ys_x.append(pos[3])
+        ys_y.append(pos[4])
+        ys_z.append(pos[5])
+
+        # Limit y lists to log_size
+        xs_roll = xs_roll[-log_size:]
+        xs_pitch = xs_pitch[-log_size:]
+        xs_yaw = xs_yaw[-log_size:]
+
+        ys_x = ys_x[-log_size:]
+        ys_y = ys_y[-log_size:]
+        ys_z = ys_z[-log_size:]
+
+        # Update the y items
+        pol0.set_xdata(xs_roll)
+        pol1.set_xdata(xs_pitch)
+        pol2.set_xdata(xs_yaw)
+
+        vecX.set_ydata(ys_x)
+        vecY.set_ydata(ys_y)
+        vecZ.set_ydata(ys_z)
     
-    xs_roll.append(pos[0]/180*np.pi)
-    xs_pitch.append(pos[1]/180*np.pi)
-    xs_yaw.append(pos[2]/180*np.pi)
-    
-    ys_x.append(pos[3])
-    ys_y.append(pos[4])
-    ys_z.append(pos[5])
-
-    # Limit y lists to log_size
-    xs_roll = xs_roll[-log_size:]
-    xs_pitch = xs_pitch[-log_size:]
-    xs_yaw = xs_yaw[-log_size:]
-
-    ys_x = ys_x[-log_size:]
-    ys_y = ys_y[-log_size:]
-    ys_z = ys_z[-log_size:]
-
-    # Update the y items
-    pol0.set_xdata(xs_roll)
-    pol1.set_xdata(xs_pitch)
-    pol2.set_xdata(xs_yaw)
-
-    vecX.set_ydata(ys_x)
-    vecY.set_ydata(ys_y)
-    vecZ.set_ydata(ys_z)
-    writer.writerow([now] + list(pos))
     return pol0, pol1, pol2, vecX, vecY, vecZ
 
 # Set serial port location and connection speed
@@ -139,12 +144,12 @@ if serial:
     ser = serial.Serial(port, baud, timeout=1)
     ser.flushInput()
     # setup plot to call animate() funciton periodically
-    ani = animation.FuncAnimation(fig, animate, fargs=(ser, now, xs_roll, xs_pitch, xs_yaw, ys_x, ys_y, ys_z), interval=1, blit=True)
+    ani = animation.FuncAnimation(fig, animate, fargs=(ser, xs_roll, xs_pitch, xs_yaw, ys_x, ys_y, ys_z), interval=1, blit=True)
     plt.show()
 else:
     ser = 0
     # setup plot to call animate() funciton periodically
-    ani = animation.FuncAnimation(fig, animate, fargs=(ser, now, xs_roll, xs_pitch, xs_yaw, ys_x, ys_y, ys_z), interval=1, blit=True)
+    ani = animation.FuncAnimation(fig, animate, fargs=(ser, xs_roll, xs_pitch, xs_yaw, ys_x, ys_y, ys_z), interval=1, blit=True)
     plt.show()
 
 f.close()
